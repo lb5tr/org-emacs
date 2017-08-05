9cbecbd9ee61588464b7f5ddcdd11c69fc657e84
lb5tr <lb5tr@lb5tr.xyz>
2017-08-05 03:47:14 +0000

    init.

---
+++ init.el
@@ -0,0 +1,30 @@
+;; stuff automatically added
+;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-use-fuzzy t)
+ '(package-selected-packages
+   (quote
+    (counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/my-init.org"))
