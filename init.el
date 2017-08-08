df90d6c88275431e92ec75e0c82bacce629c7a90
lb5tr <lb5tr@lb5tr.xyz>
2017-08-08 12:00:27 -0700

    Origin fixes

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#26292c" :foreground "#f8f8f8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :family "xos4 Terminus"))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 (require 'package)
