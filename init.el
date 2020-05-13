89500d96edb4fb35cf2bdc04c26185fb5228166b
lb5tr <lb5tr@hackerspace.pl>
2020-05-13 11:45:09 -0700

    Keep up with org file

--- init.el
+++ init.el
@@ -6,6 +6,11 @@
  '(ac-use-fuzzy t)
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (misterioso)))
+ '(custom-safe-themes
+   (quote
+    ("9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "c90fd1c669f260120d32ddd20168343f5c717ca69e95d2f805e42e88430c340e" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
+ '(fci-rule-color "#202325")
  '(package-selected-packages
    (quote
     (yafolding sml-mode geiser rainbow-delimiters paredit yaml-mode kubernetes dockerfile-mode docker omnisharp csharp-mode powershell json-mode centered-window origami company-tern company xref-js2 js2-mode protobuf-mode go-autocomplete markdown-mode helm toml-mode rust-mode nasm-mode typescript-mode tss go-mode go py-autopep8 jedi flycheck fuzzy auto-complete magit ivy counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
