a17bf23b4eaa897b5afa3927e0ad0c2efc9dccd9
lb5tr <lb5tr@hackerspace.pl>
2020-05-16 11:05:34 -0700

    add doom-modeline

--- init.el
+++ init.el
@@ -10,16 +10,14 @@
  '(custom-safe-themes
    (quote
     ("9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "c90fd1c669f260120d32ddd20168343f5c717ca69e95d2f805e42e88430c340e" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
- '(fci-rule-color "#202325")
  '(package-selected-packages
    (quote
-    (yafolding sml-mode geiser rainbow-delimiters paredit yaml-mode kubernetes dockerfile-mode docker omnisharp csharp-mode powershell json-mode centered-window origami company-tern company xref-js2 js2-mode protobuf-mode go-autocomplete markdown-mode helm toml-mode rust-mode nasm-mode typescript-mode tss go-mode go py-autopep8 jedi flycheck fuzzy auto-complete magit ivy counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
+    (doom-modeline yafolding sml-mode geiser rainbow-delimiters paredit yaml-mode kubernetes dockerfile-mode docker omnisharp csharp-mode powershell json-mode centered-window origami company-tern company xref-js2 js2-mode protobuf-mode go-autocomplete markdown-mode helm toml-mode rust-mode nasm-mode typescript-mode tss go-mode go py-autopep8 jedi flycheck fuzzy auto-complete magit ivy counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 109 :width normal :foundry "outline" :family "Consolas"))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 (require 'package)
